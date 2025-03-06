090d6df6dca4ad2727830c6e2e7f08ad57c69739
Nicolaas <github@sunnysideup.co.nz>
2025-03-06 16:09:25 +1300

    FIX: better ean checker

--- src/Api/EanChecker.php
+++ src/Api/EanChecker.php
@@ -2,6 +2,8 @@
 
 namespace Sunnysideup\EcommerceGoogleShoppingFeed\Api;
 
+use Real\Validator\Gtin\Factory;
+use Real\Validator\Gtin\NonNormalizable;
 use SilverStripe\Control\Controller;
 use SilverStripe\Control\Director;
 use SilverStripe\Core\Convert;
@@ -17,24 +19,28 @@ class EanChecker
 {
     public static function is_valid_ean(string $ean): bool
     {
-        if(strlen($ean) !== 13) {
+        if (empty($ean)) {
             return false;
         }
-        if (!preg_match("/^[0-9]{13}$/", $ean)) {
-            return false; // EAN must be 13 digits
+        try {
+            $gtin = Factory::create($ean);
+        } catch (NonNormalizable $e) {
+            return false;
         }
-
-        $sum = 0;
-        for ($i = 0; $i < 12; $i++) {
-            $sum += $ean[$i] * (($i % 2 == 0) ? 1 : 3);
+        return $gtin->checkDigit();
+    }
+    public static function format_ean(string $ean): string
+    {
+        if (self::is_valid_ean($ean)) {
+            try {
+                $gtin = Factory::create($ean);
+                if ($gtin->checkDigit()) {
+                    return $gtin->padded();
+                }
+            } catch (NonNormalizable $e) {
+                // do nothing
+            }
         }
-
-        $checkDigit = 10 - ($sum % 10);
-        if ($checkDigit == 10) {
-            $checkDigit = 0;
-        }
-
-        // Return true if the last digit matches the check digit
-        return ((int) $checkDigit == (int) $ean[12]);
+        return '';
     }
 }
